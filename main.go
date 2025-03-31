7eab59af9df077ef6bb7d16805a1aebf345c2f92
csteenberg <csteenberg@hotmail.co.uk>
2025-03-31 18:49:54 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func main() {
+	handler := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: handler,
+	}
+
+	server.ListenAndServe()
+}
